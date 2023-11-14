bf4e1c22a1f40162f7856bcbd17a59f8f4ff5250
alok12-3 <140874067+alok12-3@users.noreply.github.com>
2023-11-14 14:36:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import preprocessor,helper
+import preprocessor, helper
 import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -9,23 +9,23 @@ import plotly.figure_factory as ff
 df = pd.read_csv('athlete_events.csv')
 region_df = pd.read_csv('noc_regions.csv')
 
-df = preprocessor.preprocess(df,region_df)
+df = preprocessor.preprocess(df, region_df)
 
 st.sidebar.title("Olympics Analysis")
 
 user_menu = st.sidebar.radio(
     'Select an Option',
-    ('Medal Tally','Overall Analysis','Country-wise Analysis')
+    ('Medal Tally','Country-wise Analysis','Overall Analysis')
 )
 
 if user_menu == 'Medal Tally':
     st.sidebar.header("Medal Tally")
-    years,country = helper.country_year_list(df)
+    years, country = helper.country_year_list(df)
 
-    selected_year = st.sidebar.selectbox("Select Year",years)
+    selected_year = st.sidebar.selectbox("Select Year", years)
     selected_country = st.sidebar.selectbox("Select Country", country)
 
-    medal_tally = helper.fetch_medal_tally(df,selected_year,selected_country)
+    medal_tally = helper.fetch_medal_tally(df, selected_year, selected_country)
     if selected_year == 'Overall' and selected_country == 'Overall':
         st.title("Overall Tally")
     if selected_year != 'Overall' and selected_country == 'Overall':
@@ -36,48 +36,47 @@ if user_menu == 'Medal Tally':
         st.title(selected_country + " performance in " + str(selected_year) + " Olympics")
     st.table(medal_tally)
 
-    if user_menu == 'Overall Analysis':
+if user_menu == 'Overall Analysis':
+    st.title("Top Statistics")
+    col1, col2, col3 = st.columns(3)
 
-        st.title("Top Statistics")
-        col1, col2, col3 = st.beta_columns(3)
-        with col1:
-            st.header("Editions")
-            st.title("28")
-        with col2:
-            st.header("Hosts")
-            st.title("23")
-        with col3:
-            st.header("Sports")
-            st.title("52")
+    with col1:
+        st.header("Editions")
+        st.title("28")
+    with col2:
+        st.header("Hosts")
+        st.title("23")
+    with col3:
+        st.header("Sports")
+        st.title("52")
 
-        col1, col2, col3 = st.beta_columns(3)
-        with col1:
-            st.header("Events")
-            st.title("651")
-        with col2:
-            st.header("Nations")
-            st.title("206")
-        with col3:
-            st.header("Athletes")
-            st.title("116122")
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.header("Events")
+        st.title("651")
+    with col2:
+        st.header("Nations")
+        st.title("206")
+    with col3:
+        st.header("Athletes")
+        st.title("116122")
 
 if user_menu == 'Country-wise Analysis':
-
     st.sidebar.title('Country-wise Analysis')
 
     country_list = df['region'].dropna().unique().tolist()
     country_list.sort()
 
-    selected_country = st.sidebar.selectbox('Select a Country',country_list)
+    selected_country = st.sidebar.selectbox('Select a Country', country_list)
 
-    country_df = helper.yearwise_medal_tally(df,selected_country)
+    country_df = helper.yearwise_medal_tally(df, selected_country)
     fig = px.line(country_df, x="Year", y="Medal")
     st.title(selected_country + " Medal Tally over the years")
     st.plotly_chart(fig)
 
     st.title(selected_country + " excels in the following sports")
-    pt = helper.country_event_heatmap(df,selected_country)
+    pt = helper.country_event_heatmap(df, selected_country)
     fig, ax = plt.subplots(figsize=(20, 20))
-    ax = sns.heatmap(pt,annot=True)
+    ax = sns.heatmap(pt, annot=True)
     st.pyplot(fig)
-
